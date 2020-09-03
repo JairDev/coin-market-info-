9aeb56341e0560be563abae5b21f8c53e7137e6a
Alfredo <devalfredomoscoso@gmail.com>
2020-09-03 15:50:04 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <nav className="App-header-nav">
+          <div>Logo</div>
+          <div><a href="#">Converter</a></div>
+        </nav>
       </header>
+      <section className="App-section-headband">
+        <div>Price Coin</div>
+      </section>
+
+      <section className="App-section-coin-info">
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <th>Rank</th>
+                <th>Price</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>
+                  <div>
+                    Name
+                  </div>
+                </td>
+                <td>
+                  <div>
+                    Price
+                  </div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </section>
+      
+      <section className="App-section-news">
+        <div>News</div>
+        <div>News</div>
+      </section>
     </div>
   );
 }
