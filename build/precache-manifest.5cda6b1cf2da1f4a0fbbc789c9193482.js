bad91a6ce361f9f1a7d457385239502eb98d3a27
Alfredo <devalfredomoscoso@gmail.com>
2020-10-24 16:42:50 -0400

    Deploy

---
+++ build/precache-manifest.5cda6b1cf2da1f4a0fbbc789c9193482.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd98f53d38d454002c9b56ef9e8c7eaa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5522a76384c9e9d44a49",
+    "url": "/static/css/3.29d11159.chunk.css"
+  },
+  {
+    "revision": "ef79c5c8efa957d86d16",
+    "url": "/static/css/main.fbed637d.chunk.css"
+  },
+  {
+    "revision": "c4d9c847263d373498e8",
+    "url": "/static/js/2.f3d9a477.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f3d9a477.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5522a76384c9e9d44a49",
+    "url": "/static/js/3.7a85ac25.chunk.js"
+  },
+  {
+    "revision": "ef79c5c8efa957d86d16",
+    "url": "/static/js/main.69f13fc3.chunk.js"
+  },
+  {
+    "revision": "8eed6a8d9126af94d134",
+    "url": "/static/js/runtime-main.407e6d31.js"
+  }
+]);
